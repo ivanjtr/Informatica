8483858f5aceb2b76455fe0487b0a1a439573a41
ivanjtr <alu0100950625@ull.edu.es>
2018-10-12 20:29:26 +0100

    Se añade inicio de ejercicio 10

---
+++ ejercicio10.cpp
@@ -0,0 +1,17 @@
+// Autor: Ivan Jesus Torres Rodriguez
+// Fecha: 2018-10-10
+
+#include <vector> // para poder usar los vectores
+#include <iostream> // para usar cout y cerr
+#include <fstream> //Para acceso a ficheros
+#include <string> //Para usar el tipo string
+#include <iomanip> //Para convertir en hexadecimal
+
+std::vector<unsigned int> bits(unsigned int indiceInicial,
+    const std::vector<bool> &vector, unsigned int indiceFinal){
+  
+}
+int main(int argc,char* argv[]){
+
+
+}
